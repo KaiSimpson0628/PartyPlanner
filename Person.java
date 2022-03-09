259f0c918eadd3c8c4e75ed52b41637d4976b9b7
KaiSimpson0628 <60585848+KaiSimpson0628@users.noreply.github.com>
2022-03-09 11:06:15 -0500

    Update Person.java
    Added the names file, and manually added people

--- Person.java
+++ Person.java
@@ -1,16 +1,53 @@
-public class Person {
-int personID;
-String name;
-int companyID;
-int tableID;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
 
-public Person (int id, String pname, int coID) {
-	this.personID = id;
-	this.name = pname;
-	this.companyID = coID;
+public class Banquet {
+	
+public static void main(String[] args) {
+	
+	ArrayList<String> people = new ArrayList<String>();
+try {
+File myObj = new File("names.csv");
+Scanner scan = new Scanner(myObj);
+for (int i = 0; i<90; i++) 
+{
+	String whatever = scan.nextLine();
+	int personID = Integer.valueOf(whatever.substring(0,1));
+	people.add(whatever);
+	System.out.println(people);
+	System.out.println(personID);
+}
+	}
+catch (FileNotFoundException e) {
+System.out.println("error");
+e.printStackTrace();
+}
 
-} //Close this.person
-public String toString() {
-	return ("This persons name is " + name + "Their ID is" + personID + "Their company ID is" + companyID);
-}//close strign to String
-}//close Person
+int numPPL;
+int[][] pplfrCo = new int[16][20];
+Person[][] bt = new Person[10][10];
+Person p1 = new Person(3, "Kai",  13);
+System.out.println(p1);
+Person p2 = new Person(5, "John",  18);
+System.out.println(p2);
+Person p3 = new Person(8, "Felix",  8);
+System.out.println(p3);
+Person p4 = new Person(2, "Abigail",  10);
+System.out.println(p4);
+Person p5 = new Person(7, "Neel",  15);
+System.out.println(p5);
+Person p6 = new Person(6, "Will",  7);
+System.out.println(p6);
+Person p7 = new Person(1, "Max",  2);
+System.out.println(p7);
+Person p8 = new Person(4, "Jerry",  4);
+System.out.println(p8);
+Person p9 = new Person(9, "Jofree",  12);
+System.out.println(p9);
+Person p10 = new Person(10, "Marcus",  3);
+System.out.println(p10);
+
+}
+}
