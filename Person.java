fd61b78eab4f64572adf84cdc973d2be105b7314
KaiSimpson0628 <60585848+KaiSimpson0628@users.noreply.github.com>
2022-03-24 14:20:04 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -14,3 +14,16 @@ public String toString() {
 	return ("This persons name is " + name + ", their ID is " + personID + ", their company ID is " + companyID);
 }
 }
+
+public void scan()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Person's name is ");
+		personName = scan.nextLine();
+		System.out.println("person's ID is");
+		personID = scan.nextInt();
+		System.out.println("Company's ID is");
+		companyID = scan.nextInt();
+		
+	}
+	
